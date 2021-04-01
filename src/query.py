1a014d47f427674272989964c9b80066318ada1d
mmcentre <michael.bouzinier@kvitneset.com>
2021-04-01 00:09:07 -0400

    MAB   Adding query exercise

--- src/query.py
+++ src/query.py
@@ -24,50 +24,27 @@
 #  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-import gzip
 import logging
-import sys
-
 from src.connection import connect
-from src.introspection import get_columns
 
 
-def import_csv(path: str):
-    # *******************************************************
-    columns = get_columns(path)
-    column_list = ',\n'.join([
-        "\t{} {}".format(c, columns[c])
-        for c in columns
-    ])
-    table_ddl = "CREATE TABLE IF NOT EXISTS data_exploration (\n{}\n)"\
-        .format(column_list)
-    logging.info(table_ddl)
-    # ********************************************************
-
-    sql = "COPY data_exploration FROM STDIN WITH " \
-          + "(FORMAT CSV, DELIMITER '\t', " \
-          + "NULL '(null)', HEADER TRUE, QUOTE '\"')"  # QUOTE requires expert mode
-    # *********************************************************
-
-    index_ddl = "CREATE INDEX IF NOT EXISTS de_{name}_idx ON data_exploration ({name})"
-
+def query():
     with connect() as connection:
         cursor = connection.cursor()
-        cursor.execute(table_ddl)
-        # *************************
+        cursor.execute("""
+        SELECT COUNT(*) FROM data_exploration
+        """)
+        for row in cursor:
+            print(row)
 
-        with gzip.open(path, "rt") as stream:
-            next(stream)
-            cursor.copy_expert(sql, stream)
-        # *************************
-
-        for c in columns:
-            if c not in ['year', 'county_fips'] and columns[c] != "VARCHAR":
-                continue
-            logging.info(index_ddl.format(name=c))
-            cursor.execute(index_ddl.format(name=c))
+        cursor.execute("""
+        SELECT DISTINCT county_name FROM data_exploration
+        WHERE state = 'MA'
+        """)
+        for row in cursor:
+            print(row)
 
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
-    import_csv(sys.argv[1])
+    query()
